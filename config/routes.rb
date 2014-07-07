c8d155579d565c0ecd3a80ad359e163437308182
mdmatheus <md.matheus@gmail.com>
2014-07-06 23:14:09 -0400

    Add layout links with routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  root 'pages#home'
+  match '/contact', :to => 'pages#contact', via: 'get'
+  match '/about',   :to => 'pages#about', via: 'get'
+  match '/help',    :to => 'pages#help', via: 'get'
   get 'pages/home'
-  get 'pages/about'
-  get 'pages/contact'
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe PagesController do
   render_views
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
 
   describe "GET 'home'" do
     it "returns http success" do
@@ -11,7 +13,7 @@ describe PagesController do
     
     it "should have the right title" do
       get 'home'
-      response.should have_selector("title", :content => "Ruby on Rails Tutorial App | Home")
+      response.should have_selector("title", :content => "#{@base_title} | Home")
     end
     
     it "should have a non-blank body" do
@@ -28,7 +30,7 @@ describe PagesController do
     
     it "should have the right title" do
       get 'contact'
-      response.should have_selector("title", :content => "Ruby on Rails Tutorial App | Contact")
+      response.should have_selector("title", :content => "#{@base_title} | Contact")
     end
   end
   
@@ -40,7 +42,19 @@ describe PagesController do
     
     it "should have the right title" do
       get 'about'
-      response.should have_selector("title", :content => "Ruby on Rails Tutorial App | About")
+      response.should have_selector("title", :content => "#{@base_title} | About")
+    end
+  end
+   
+   describe "GET 'help'" do
+    it "returns http success" do
+      get 'help'
+      response.should be_success
+    end
+    
+    it "should have the right title" do
+      get 'help'
+      response.should have_selector("title", :content => "#{@base_title} | Help")
     end
   end
 
